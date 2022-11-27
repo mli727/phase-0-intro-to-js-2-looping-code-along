e388b4cf42438c3337d131a6b7a82723ab49af2a
Michael Li <li.michael720@gmail.com>
2022-11-26 23:27:01 -0500

    commit tree dc15d8265488b5744da1c8a2a367a7a2e52acad2

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(cards,event) {
+    const newCards = []
+    for (let i = 0; i < cards.length; i++) {
+        newCards.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`);
+    }
+    return newCards
+}
+
+function countDown(x) {
+    let counter = x
+    while (counter >= 0) {
+        console.log(counter--);
+    }
+}
